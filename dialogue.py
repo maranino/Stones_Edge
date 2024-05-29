d27904dc9ca9ed0586af043f3cc35e972d061c6a
Zynkah <ffgurl07@yahoo.com>
2024-05-28 20:53:39 -0500

    Fix: additions to characters

---
+++ dialogue.py
@@ -0,0 +1,13 @@
+def intro():
+    '''intro of the game'''
+    print("""
+        ->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
+
+            There has been a rumor floating around town...    
+            of a rare jewel the King lost on his last journey.
+                            Problem is...    
+            the details seem to change depending on who you ask.
+            You have four different options of where to go...    
+                        Which will you choose? 
+
+        ->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->""")
--- stones_edge.py
+++ stones_edge.py
@@ -2,36 +2,76 @@ import random
 from datetime import date
 
 from screens import characterScreen, locationsScreen, travelersScreen
-from art import castle, mountains, forest, desert, swamps, gameOver, cross, diceArt
+from dialogue import intro
+from art import castle, mountains, forest, desert, swamps, gameOver, cross, diceArt, youWin
 
-class Character:
-    '''This class is to define each character's name, hit point, attack damage and thier job title'''
 
-    def __init__(self, name, hp, damage, job):
+class Characters:
+    '''parent class for characters'''
+    def __init__(self, name, hp, mp, damage, magic, magic_defense, luck):
         self.name = name
         self.hp = hp
+        self.mp = mp
         self.damage = damage
+        self.magic = magic
+        self.magic_defense = magic_defense
+        self.luck = luck
+
+class Inventory:
+    '''This class is to define the inventory for each character'''
+
+    def __init__(self, armor, weapon, hp_potion, mp_potion, drops, gathers, gift):
+        self.armor = armor
+        self.weapon = weapon
+        self.hp_potion = hp_potion
+        self.mp_potion = mp_potion
+        self.drops = drops
+        self.gathers = gathers
+        self.gift = gift
+
+kiri_inventory = Inventory('pelt', 'scythe', 40, 40, '', '', '')
+mika_inventory = Inventory('vest', 'dagger', 40, 40, '', '', '')
+rusty_inventory = Inventory('crown', 'saber', 40, 40, '', '', '')
+ciel_inventory = Inventory('shield', 'broadsword', 40, 40, '', '', '')
+class Character(Characters):
+    '''This class is to define each character's name, hit point, attack damage and their job title'''
+
+    def __init__(self, name, job, hp, mp, damage, magic, magic_defense, luck, inventory):
         self.job = job
+        self.inventory = inventory
+        super().__init__(name, hp, mp, damage, magic, magic_defense, luck)
 
-kiri = Character('Kiri', 140, 60, 'farmer')
-mika = Character('Mika', 120, 70, 'merchant')
-rusty = Character('Rusty', 130, 80, 'prince')
-ciel = Character('Ciel', 120, 70, 'knight')
 
-class Monsters:
+kiri = Character('Kiri','farmer', 140, 100, 60, 'earth', 50, 15, kiri_inventory)
+mika = Character('Mika', 'merchant', 120, 110, 70, 'lightning', 60, 25, mika_inventory)
+rusty = Character('Rusty', 'prince', 130, 90, 80, 'water', 40, 30, rusty_inventory)
+ciel = Character('Ciel', 'knight', 120, 80, 70, 'fire', 30, 20, ciel_inventory)
+
+class Monsters(Characters):
     '''This class is to define each monster's name, hit points and attack damage'''
 
-    def __init__(self, name, hp, damage):
+    def __init__(self, name, hp, damage, mp, magic, magic_defense, luck, drops):
+        self.drops = drops
+        super().__init__(name, hp, mp, damage, magic, magic_defense, luck)
+
+
+serpent = Monsters('Serpent', 190, 30, 40, 'earth', 50, 4, 'scales')
+hawk = Monsters('Hawk', 180, 40, 40, 'wind', 50, 3, 'feathers')
+bear = Monsters('Bear', 220, 50, 50, 'ice', 40, 2, 'fur')
+gator = Monsters('Gator', 200, 40, 30, 'water', 30, 6, 'teeth')
+
+class Traveler:
+    '''This class is to specify the details of the traveler'''
+    def __init__(self, name, gift):
         self.name = name
-        self.hp = hp
-        self.damage = damage
+        self.gift = gift
 
-serpant = Monsters('Serpant', 190, 30)
-hawk = Monsters('Hawk', 180, 40)
-bear = Monsters('Bear', 220, 50)
-gator = Monsters('Gator', 200, 40)
+tristan = Traveler('Tristan', 'corn dolly')
+mark = Traveler('Mark', 'amulet')
+trinity = Traveler('Trinity', 'voodoo doll')
+juni = Traveler('Juni', 'seer stone')
 
-class Locations:
+class Locations():
     '''This class is to define each location's direction, terrain, 
     the traveler, and the cover, trap and monument found in the story'''
 
@@ -43,10 +83,10 @@ class Locations:
         self.trap = trap
         self.monument = monument
 
-east = Locations('east', 'forest', 'Tristan', 'tree', 'into a pit of spikes', 'totem pole')
-west = Locations('west', 'desert', 'Mark', 'rock', 'into a pit of snakes', 'obelisk')
-south = Locations('south', 'swamps', 'Trinity', 'broken tree', 'into quicksand', 'tomb')
-north = Locations('north', 'mountains', 'Juni', 'boulder', 'off a cliff', 'temple')
+east = Locations('east', 'forest', tristan.name, 'tree', 'into a pit of spikes', 'totem pole')
+west = Locations('west', 'desert', mark.name, 'rock', 'into a pit of snakes', 'obelisk')
+south = Locations('south', 'swamps', trinity.name, 'broken tree', 'into quicksand', 'tomb')
+north = Locations('north', 'mountains', juni.name, 'boulder', 'off a cliff', 'temple')
 
 beginningDate = date(1556, 6, 20)
 earlyDeath = date(1556, 6, 21)
@@ -61,6 +101,7 @@ journeyTime = {'journey1' : endDate.day - beginningDate.day,
                }
 
 def gameplay():
+    '''Play the game'''
     while True:
         while True:
             # Choose the Character:
@@ -91,7 +132,7 @@ def gameplay():
             else:
                 print("Invalid entry, try again!")
 
-        print(f"""    
+        print(f"""
     ->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
 
                         You have chosen {character.name}
@@ -107,18 +148,7 @@ def gameplay():
     ->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
         """)
         castle()
-        print(f"""
-    ->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
-
-        There has been a rumor floating around town...    
-        of a rare jewel the King lost on his last journey.
-                        Problem is...    
-        the details seem to change depending on who you ask.
-        You have four different options of where to go...    
-                    Which will you choose? 
-
-    ->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
-        """)
+        intro()
         while True:
             # Choose the Location
             locationsScreen()
@@ -139,7 +169,7 @@ def gameplay():
 
             elif choice == "desert" or choice == "west":
                 location = west
-                monster = serpant
+                monster = serpent
                 desert()
                 break
 
@@ -169,8 +199,8 @@ def gameplay():
                 The {monster.name}'s hit points are now: {str(monster.hp)}
         """)
             if monster.hp > 0:
-                    character.hp -= monster.damage
-                    print(f"""
+                character.hp -= monster.damage
+                print(f"""
                     The {monster.name} strikes back at {character.name}!
                     {character.name}'s hit points are now: {str(character.hp)}
         """)
@@ -208,7 +238,6 @@ def gameplay():
             travelersScreen()
             choice = input("""
                   Will you approach the Traveler? """).lower()
-            
             if choice == "yes" or choice == "y":
                 print(f"""
     ->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
@@ -217,11 +246,11 @@ def gameplay():
                 They says their name is {location.traveler}.
         The traveler sees you are wounded and asks... 
             why you are outside the walls of the Royal City.
-            You tell {location.traveler} of the {monster.name} attack 
+            You tell {location.traveler} of the {monster.name} attack
                     and how you barely escaped...
             Then you tell them of the rumor you heard 
                 and ask if they had known anything.
-                        {location.traveler} says they have, 
+                        {location.traveler} says they have,
             but first you must win a game of their choice.
                             You agree.
 
@@ -259,18 +288,9 @@ def gameplay():
         You have succeeded in your adventure on {gameDate.strftime('%B')} {gameDate.day}, {gameDate.year}!
                  Your journey lasted {journeyTime['journey3']} days.
         
-    ->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
-         __   __ _______ __   __    _     _ ___ __    _ __ 
-        |  | |  |       |  | |  |  | | _ | |   |  |  | |  |
-        |  |_|  |   _   |  | |  |  | || || |   |   |_| |  |
-        |       |  | |  |  |_|  |  |       |   |       |  |
-        |_     _|  |_|  |       |  |       |   |  _    |__|
-          |   | |       |       |  |   _   |   | | |   |__ 
-          |___| |_______|_______|  |__| |__|___|_|  |__|__|
 
-    ->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
 
-                    """, f"""
+                    """,youWin(), f"""
 
                       An old coin? 
     {location.traveler} places it into your hand and you evaluate it.
@@ -302,7 +322,7 @@ def gameplay():
     I suppose I will look further in the {location.terrain} for clues.
                 You say your farewells and continue on.
                 You go deeper into the {location.terrain}...
-        Back home you heard that there was a {location.monument} 
+        Back home you heard that there was a {location.monument}
                 that held a clue of the jewels location!
                 If you could just find this, 
             surely you will find what you are looking for...
@@ -312,7 +332,7 @@ def gameplay():
     ->->->->->->->->->->->->->->->->->->->->->->->->->->->->->->
 
                 You stumble around for a while 
-                and almost fall {location.trap}! 
+                and almost fall {location.trap}!
             Thankfully you saw it last minute! 
         Being a little more cautious now you continue on...
             As the sun starts to set you begin to worry
@@ -321,7 +341,7 @@ def gameplay():
               and figure since it worked last time,
         you should try hiding in a spot like that again!
     You get closer to look around and make sure it's all clear.
-          As you peak your head around the {location.cover} 
+          As you peak your head around the {location.cover}
          you see the {location.monument} you were looking for!
                  You quickly move towards it 
               to look for the clue you heard of!
@@ -330,7 +350,7 @@ def gameplay():
         """)
                 chance = [f"There are no hints or clues at this {location.monument}!", f"The jewel you look for has already been taken by {location.traveler}!"]
                 clue = random.choice(chance)
-                print(f""" 
+                print(f"""
       There was always a chance that the rumors weren't true.
         This moment will determine the fate of my adventure!
         If I get no new leads here, then I must return home.
@@ -353,7 +373,7 @@ def gameplay():
                 You do not have the strength to fight 
                 after that battle with the {monster.name}!
             Instead you go deeper into the {location.terrain}...
-            Back home you heard that there was a {location.monument} 
+            Back home you heard that there was a {location.monument}
                 that held a clue of the jewels location!
                     If you could just find this, 
             surely you will find what you are looking for...
@@ -361,7 +381,7 @@ def gameplay():
         you pull it out and see if you could find your way...
                     You slowly step forward as you 
             try to trace your steps from the city...
-                    Suddenly!! You step {location.trap}!! 
+                    Suddenly!! You step {location.trap}!!
 
                   You have died on {trapDeath.strftime('%B')} {trapDeath.day}, {trapDeath.year}!
                   Your journey lasted {journeyTime['journey4']} days.
@@ -380,11 +400,12 @@ def gameplay():
 
 # Code for replay the game
 def replay():
-    replay = input("""
+    '''replay the game?'''
+    play = input("""
                             Play again? """).lower()
-    if replay == "yes" or replay == "y":
+    if play == "yes" or play == "y":
         gameplay()
-    if replay == "no" or replay == "n":
+    if play == "no" or play == "n":
         gameOver()
     quit()
 
